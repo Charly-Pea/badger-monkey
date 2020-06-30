9dc55c5ec57fbc1536a24d5623c2e4bd6efa6748
Charly-Pea <60979897+Charly-Pea@users.noreply.github.com>
2020-06-30 09:22:55 +0100

    Add files via upload

---
+++ blackout.js
@@ -0,0 +1,19 @@
+if (localStorage['blackout']) {
+ if (Number(localStorage['blackout']) == 1) {
+$('BODY').addClass('blackout');
+ } else {
+  $('BODY').removeClass('blackout');
+ }
+} else {
+  localStorage['blackout'] = 0;
+  $('BODY').removeClass('blackout');
+}
+$('BODY').show();
+$('#boToggle').on('click', function(){
+  if (Number(localStorage['blackout']) == 0) {
+      localStorage['blackout'] = 1;
+$('BODY').addClass('blackout');
+  } else {
+      localStorage['blackout'] = 0;
+$('BODY').removeClass('blackout');
+ }
